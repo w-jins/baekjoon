7cd35146d20d93ebe66eeed8a58d6dcc3cd62ad4
JIN <jinneen@naver.com>
2025-11-04 22:50:49 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 116 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(n + "\n" + 1);
+        bw.flush();
+        bw.close();
+    }
+
+}
